b38032fa6440078cc4f54a3995ad9961330ed73a
Filipe Moreira <filimor@posteo.com.br>
2020-07-08 18:43:00 -0300

    Fix syntax errors

--- App/controllers/indexController.php
+++ App/controllers/indexController.php
@@ -6,12 +6,12 @@ use MF\Controller\Action;
 
 class IndexController extends Action {
     public function index() {
-        $this->view->$dados = array('Sofa', 'Cadeira', 'Cama');
+        $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
         $this->render('index');
     }
 
     public function sobreNos() {
-        $dados = array('Sofa', 'Cadeira', 'Cama');
+        $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
         $this->render('sobreNos');
     }
 }
