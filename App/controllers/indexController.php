9418be8b054e7f741690944182cacc8f06f8fd43
Filipe Moreira <filimor@posteo.com.br>
2020-07-08 19:10:35 -0300

    Define layouts

--- App/controllers/indexController.php
+++ App/controllers/indexController.php
@@ -7,12 +7,12 @@ use MF\Controller\Action;
 class IndexController extends Action {
     public function index() {
         $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
-        $this->render('index');
+        $this->render('index', 'layout1');
     }
 
     public function sobreNos() {
         $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
-        $this->render('sobreNos');
+        $this->render('sobreNos','layout1');
     }
 }
 
