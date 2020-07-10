b1f7893ee27abd60830234f23abcc3e077285ae7
Filipe Moreira <filimor@posteo.com.br>
2020-07-10 18:44:49 -0300

    Improve controllers and fix a syntax bug

--- App/controllers/indexController.php
+++ App/controllers/indexController.php
@@ -3,15 +3,22 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use MF\Model\Container;
+use App\Models\Produto;
+use App\Models\Info;
 
 class IndexController extends Action {
+    
     public function index() {
-        $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
+
+        $produto = Container::getModel('Produto');
+        $this->view->dados = $produto->getProdutos();
         $this->render('index', 'layout1');
     }
 
     public function sobreNos() {
-        $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
+        $info = Container::getModel('Info');
+        $this->view->dados = $info->getInfo();
         $this->render('sobreNos','layout1');
     }
 }
